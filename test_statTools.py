ed943778eb19b4d145cf027f3b5d64593ed9bd52
Felix <felix.yang19@ycdsbk12.ca>
2018-11-01 12:27:15 -0400

    Template for Test Cases

--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,20 @@
+# Test 1:
+intList1 = []
 
+# Test 2:
+intList2 = []
+
+# Test 3:
+intList3 = []
+
+# Test 4:
+intList4 = []
+
+# Test 5:
+intList5 = []
+
+# Corner 1: Empty List:
+cornerList1 = []
+
+# Corner 2:
+cornerList2 = []
