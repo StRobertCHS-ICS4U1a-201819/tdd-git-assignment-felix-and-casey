91462e4fe2425b0d35ba9024b42e2df51c74ae6d
Felix <felix.yang19@ycdsbk12.ca>
2018-11-01 14:57:17 -0400

    Added the function descriptions, change to Docstrings later

--- statTools.py
+++ statTools.py
@@ -1,27 +1,35 @@
 # Central Tendency Functions
 
+# Sum of all terms / # of terms
 def mean(intList):
     return 1
 
+# Number that appears the most
 def mode(intList):
     return 1
 
+# Middle number of the sorted list
 def median(intList):
     return 1
 
 # Spread Functions
 
+# Largest - Smallest
 def range(intList):
     return 1
 
+# Median of the numbers below the median
 def lwrQuartile(intList):
     return 1
 
+# Median of the numbers above the median
 def uprQuartile(intList):
     return 1
 
+# Average of the squares of the differences of each number from the mean
 def variance(intList):
     return 1
 
+# Square root of the variance
 def stdDeviation(intList):
     return 1
