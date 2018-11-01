0e763c79c99ae603c7758738cf05c9d0f17cd9e0
Felix <felix.yang19@ycdsbk12.ca>
2018-11-01 10:27:07 -0400

    All basic functions with base 1 return

--- statTools.py
+++ statTools.py
@@ -5,3 +5,23 @@ def mean():
 
 def mode():
     return 1
+
+def median():
+    return 1
+
+# Spread Functions
+
+def range():
+    return 1
+
+def lwrQuartile():
+    return 1
+
+def uprQuartile():
+    return 1
+
+def variance():
+    return 1
+
+def stdDeviation():
+    return 1
