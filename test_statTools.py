8025d2c326fe8ebea1fed102f479e78577502ab8
Felix <felix.yang19@ycdsbk12.ca>
2018-11-02 09:17:18 -0400

    MeanB1 Test Fail

--- test_statTools.py
+++ test_statTools.py
@@ -13,11 +13,13 @@ intList3 = [9,1,3, 8, 0, 5, 2, 2, 6, 2, 2]
 # Test 4: Non-sorted W Negatives:
 intList4 = [5, 5, -5, 5, -5, 10, -9, 7, 8, 1, 3]
 
-# Test 5:
-intList5 = []
-
 # Corner 1: Empty List:
 cornerList1 = []
 
 # Corner 2: No Mode:
 cornerList2 = [-1, 0, 1, 3, 5, 6, 8, 9]
+
+
+# Testing Methods:
+def test_meanB1():
+    assert (5.5 == mean(intList1))
