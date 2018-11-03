e21b7d9f966a142c11a819ce4551af2150defe52
Felix <felix.yang19@ycdsbk12.ca>
2018-11-03 19:38:49 -0400

    All mean tests made + 1 extra test list

--- test_statTools.py
+++ test_statTools.py
@@ -4,21 +4,23 @@ import pytest
 # Test 1: Sorted:
 intList1 = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10]
 
-# Test 2: Sorted W Negatives:
+# Test 2: Sorted W negatives:
 intList2 = [-100, -69, -34, -10, -10, -1, 2, 10, 11, 100]
 
 # Test 3: Non-sorted list:
 intList3 = [9,1,3, 8, 0, 5, 2, 2, 6, 2, 2]
 
-# Test 4: Non-sorted W Negatives:
+# Test 4: Non-sorted W negatives:
 intList4 = [5, 5, -5, 5, -5, 10, -9, 7, 8, 1, 3]
 
-# Corner 1: Empty List:
+# Corner 1: Empty list:
 cornerList1 = []
 
-# Corner 2: No Mode:
+# Corner 2: Multiple mode:
 cornerList2 = [-1, 0, 1, 3, 5, 6, 8, 9]
 
+# Corner 3: No lower/upper quartile:
+cornerList3 = [0]
 
 # Testing Methods:
 def test_meanB1():
@@ -26,3 +28,18 @@ def test_meanB1():
 
 def test_meanB2():
     assert (-10.1 == mean(intList2))
+
+def test_meanB3():
+    assert (40/11 == mean(intList3))
+
+def test_meanB4():
+    assert (25/11 == mean(intList4))
+
+def test_meanC1():
+    assert (0 == mean(cornerList1))
+
+def test_meanC2():
+    assert (31/8 == mean(cornerList2))
+
+def test_meanC3():
+    assert (0 == mean(cornerList3))
